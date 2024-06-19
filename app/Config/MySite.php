e1a701e16295cc5336f5580ae1811e6438ee2fe0
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2024-06-19 16:13:33 +0800

    tutor1

---
+++ app/Config/MySite.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class MySite extends BaseConfig
+{
+    public $siteName  = "Sakukode";
+    public $siteEmail = "sakukode@gmail.com";
+}
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,11 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+
+
+    public function index()
     {
-        return view('welcome_message');
+        $config = config('MySite');
+        echo "Selamat Datang di {$config->siteName}. Kirim kritik dan saran ke email {$config->siteEmail}";
     }
 }
